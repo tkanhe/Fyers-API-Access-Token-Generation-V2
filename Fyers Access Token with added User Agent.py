fea7135ab7497ee7764ea7a3515a1f183533f1ef
TK <tkanhe@gmail.com>
2021-12-16 23:31:49 +0530

    Added assert statements to check errors efficiently.

--- Fyers Access Token with added User Agent.py
+++ Fyers Access Token with added User Agent.py
@@ -40,16 +40,19 @@ def setup():
 
     data1 = f'{{"fy_id":"{username}","password":"{password}","app_id":"2","imei":"","recaptcha_token":""}}'
     r1 = s.post('https://api.fyers.in/vagator/v1/login', data=data1)
+    assert r1.status_code == 200, f'Error in r1:\n {r1.json()}'
     request_key = r1.json()["request_key"]
 
     data2 = f'{{"request_key":"{request_key}","identity_type":"pin","identifier":"{pin}","recaptcha_token":""}}'
     r2 = s.post('https://api.fyers.in/vagator/v1/verify_pin', data=data2)
+    assert r2.status_code == 200, f'Error in r2:\n {r2.json()}'
 
     headers = {'authorization': f"Bearer {r2.json()['data']['access_token']}",
                'content-type': 'application/json; charset=UTF-8'}
 
     data3 = f'{{"fyers_id":"{username}","app_id":"{app_id}","redirect_uri":"{redirect_uri}","appType":"100","code_challenge":"","state":"abcdefg","scope":"","nonce":"","response_type":"code","create_cookie":true}}'
     r3 = s.post('https://api.fyers.in/api/v2/token', headers=headers, data=data3)
+    assert r3.status_code == 308, f'Error in r3:\n {r3.json()}'
 
     parsed = urlparse(r3.json()['Url'])
     auth_code = parse_qs(parsed.query)['auth_code'][0]
