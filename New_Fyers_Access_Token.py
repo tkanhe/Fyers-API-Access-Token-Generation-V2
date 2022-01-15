5ee08c2979aaa2ec210dfdf4810f3bdf7dc8a072
tkanhe <tkanhe@gmail.com>
2022-01-15 11:31:27 +0530

    Black Formatting

--- New_Fyers_Access_Token.py
+++ New_Fyers_Access_Token.py
@@ -22,41 +22,46 @@ def read_file():
 
 
 def write_file(token):
-    with open('tokenf.txt', 'w') as f:
+    with open("tokenf.txt", "w") as f:
         f.write(token)
 
 
 def setup():
-    session = accessToken.SessionModel(client_id=client_id, secret_key=secret_key, redirect_uri=redirect_uri,
-                                       response_type='code', grant_type='authorization_code')
+    session = accessToken.SessionModel(
+        client_id=client_id,
+        secret_key=secret_key,
+        redirect_uri=redirect_uri,
+        response_type="code",
+        grant_type="authorization_code",
+    )
 
     s = requests.Session()
 
     data1 = f'{{"fy_id":"{username}","password":"{password}","app_id":"2","imei":"","recaptcha_token":""}}'
-    r1 = s.post('https://api.fyers.in/vagator/v1/login', data=data1)
-    assert r1.status_code == 200, f'Error in r1:\n {r1.json()}'
+    r1 = s.post("https://api.fyers.in/vagator/v1/login", data=data1)
+
+    assert r1.status_code == 200, f"Error in r1:\n {r1.json()}"
+
     request_key = r1.json()["request_key"]
 
     data2 = f'{{"request_key":"{request_key}","identity_type":"pin","identifier":"{pin}","recaptcha_token":""}}'
-    r2 = s.post('https://api.fyers.in/vagator/v1/verify_pin', data=data2)
-    assert r2.status_code == 200, f'Error in r2:\n {r2.json()}'
-    headers = {
-        'authorization': f"Bearer {r2.json()['data']['access_token']}",
-        'content-type': 'application/json; charset=UTF-8'
-    }
+    r2 = s.post("https://api.fyers.in/vagator/v1/verify_pin", data=data2)
 
+    assert r2.status_code == 200, f"Error in r2:\n {r2.json()}"
+
+    headers = {"authorization": f"Bearer {r2.json()['data']['access_token']}", "content-type": "application/json; charset=UTF-8"}
     data3 = f'{{"fyers_id":"{username}","app_id":"{app_id}","redirect_uri":"{redirect_uri}","appType":"100","code_challenge":"","state":"abcdefg","scope":"","nonce":"","response_type":"code","create_cookie":true}}'
+    r3 = s.post("https://api.fyers.in/api/v2/token", headers=headers, data=data3)
 
-    r3 = s.post('https://api.fyers.in/api/v2/token', headers=headers, data=data3)
-    assert r3.status_code == 308, f'Error in r3:\n {r3.json()}'
+    assert r3.status_code == 308, f"Error in r3:\n {r3.json()}"
 
-    parsed = urlparse(r3.json()['Url'])
-    auth_code = parse_qs(parsed.query)['auth_code'][0]
+    parsed = urlparse(r3.json()["Url"])
+    auth_code = parse_qs(parsed.query)["auth_code"][0]
     session.set_token(auth_code)
     response = session.generate_token()
     token = response["access_token"]
     write_file(token)
-    print('Got the access token!!!')
+    print("Got the access token!!!")
     fyers = fyersModel.FyersModel(client_id=client_id, token=token, log_path=os.getcwd())
     print(fyers.get_profile())
 
@@ -65,18 +70,18 @@ def check():
     try:
         token = read_file()
     except FileNotFoundError:
-        print('Getting the access token!')
+        print("Getting the access token!")
         setup()
         sys.exit()
     fyers = fyersModel.FyersModel(client_id=client_id, token=token, log_path=os.getcwd())
     response = fyers.get_profile()
-    if 'error' in response['s'] or 'error' in response['message'] or 'expired' in response['message']:
-        print('Getting a access token!')
+    if "error" in response["s"] or "error" in response["message"] or "expired" in response["message"]:
+        print("Getting a access token!")
         setup()
     else:
-        print('You already have a access token!')
+        print("You already have a access token!")
         print(response)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     check()
