fd5136ac3455de97c101fd471d96e425b8ac4beb
tkanhe <tkanhe@collegedoors.com>
2023-04-26 19:53:34 +0530

    redirect for old removed files

--- Fyers_Access_Token_with_User_Agent.py
+++ Fyers_Access_Token_with_User_Agent.py
@@ -1,6 +1,9 @@
 """
-The file has been moved to 
+The file has been moved to repo:
 https://github.com/tkanhe/Fyers-API-Access-Token-Generation-V2. 
+
+https://github.com/tkanhe/Fyers-API-Access-Token-Generation-V2/blob/main/fyers_access_token_totp.py
+https://github.com/tkanhe/Fyers-API-Access-Token-Generation-V2/blob/main/fyers_access_token_password.py
 
 Inconvenience caused in regretted.
 """
