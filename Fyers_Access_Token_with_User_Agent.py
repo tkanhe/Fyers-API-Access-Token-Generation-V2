d14f720e7523435cf11f30b8635eaaf3d1049414
Tushar Kanhe <tkanhe@gmail.com>
2023-04-26 20:04:03 +0530

    Update Fyers_Access_Token_with_User_Agent.py

--- Fyers_Access_Token_with_User_Agent.py
+++ Fyers_Access_Token_with_User_Agent.py
@@ -1,7 +1,7 @@
 """
 This script has been deprecated. Please use the following scripts in the repo https://github.com/tkanhe/Fyers-API-Access-Token-Generation-V2
 
-To generate access token using TOTP:
+To generate access token using TOTP (Recommended):
 https://github.com/tkanhe/Fyers-API-Access-Token-Generation-V2/blob/main/fyers_access_token_totp.py
 
 To generate access token using password:
